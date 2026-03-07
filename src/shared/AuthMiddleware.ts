3398f049be1cf7dc445511c14046c654867c7c42
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-03-07 17:05:41 -0500

    Fixed issue where empty body resulted in 500

--- src/shared/AuthMiddleware.ts
+++ src/shared/AuthMiddleware.ts
@@ -3,7 +3,8 @@ import { Context, Next } from "hono";
 import { ValidateAuthorizationUseCase } from "./domain/usecase/ValidateAuthorizationUseCase";
 
 export const AuthMiddleware = async (context: Context, next: Next) => {
-    const body = await context.req.raw.clone().json() as object; // Clone the request to avoid consuming the body stream, allowing downstream handlers to read it as well
+    const rawBody = await context.req.raw.clone().json().catch(() => null); // Clone the request to avoid consuming the body stream, allowing downstream handlers to read it as well
+    const body = rawBody as object;
     const headers = context.req.header();
 
     await new ValidateAuthorizationUseCase().execute(headers, body);
