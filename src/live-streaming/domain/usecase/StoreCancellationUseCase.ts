0fc6010d439c05579e7c8ea6ebdcadd9e6ad5939
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-03-13 16:20:45 -0400

    Add request validation for the timeOfEvent property

--- src/live-streaming/domain/usecase/StoreCancellationUseCase.ts
+++ src/live-streaming/domain/usecase/StoreCancellationUseCase.ts
@@ -1,12 +1,18 @@
+import { NotAValidCancelEventPayloadError } from "../model/NotAValidCancelEventPayloadError";
 import { PersistentDataSource } from "../../data/datasource/PersistentDataSource";
 
 export class StoreCancellationUseCase {
     constructor(
         d1: D1Database,
-        private readonly dataSource: PersistentDataSource = new PersistentDataSource(d1)
+        private readonly dataSource: PersistentDataSource = new PersistentDataSource(d1),
+        private readonly now: () => Date = () => new Date()
     ) { }
 
     async execute(name: string, cancellationReason: string, timeOfEvent: string): Promise<void> {
+        if (new Date(timeOfEvent) <= this.now()) {
+            throw new NotAValidCancelEventPayloadError("The cancellation.timeOfEvent must be in the future");
+        }
+
         await this.dataSource.createOrUpdateCancellation(name, cancellationReason, timeOfEvent);
     }
 }
