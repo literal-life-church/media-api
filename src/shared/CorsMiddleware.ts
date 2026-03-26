f22c10b51834e70ea3da0d01c4c3a8b2fbc0f84e
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-03-26 14:16:08 -0400

    Doesn't send CORS header when it is unnecessary

--- src/shared/CorsMiddleware.ts
+++ src/shared/CorsMiddleware.ts
@@ -3,25 +3,25 @@ import { Context, Next } from "hono";
 import { CORS_ALLOWED_HEADERS, CORS_ALLOWED_METHODS, CORS_ALLOWED_ORIGINS, CORS_VARY, X_POWERED_BY } from "./config";
 
 export const CorsMiddleware = async (context: Context, next: Next) => {
-    const allowedOrigins = cleanCommaList(CORS_ALLOWED_ORIGINS);
-
-    const requestOrigin = context.req.header("origin");
-    const matchedOrigin = requestOrigin && allowedOrigins.includes(requestOrigin)
-        ? requestOrigin
-        : "null";
-
-    context.header("Access-Control-Allow-Credentials", "true");
-    context.header("Access-Control-Allow-Headers", cleanCommaList(CORS_ALLOWED_HEADERS).join(", "));
-    context.header("Access-Control-Allow-Methods", cleanCommaList(CORS_ALLOWED_METHODS).join(", "));
-    context.header("Access-Control-Allow-Origin", matchedOrigin);
-
     context.header("Connection", "keep-alive");
     context.header("Vary", cleanCommaList(CORS_VARY).join(", "), { append: true });
     context.header("X-Powered-By", X_POWERED_BY);
 
-    // For browser preflight requests
-    if (context.req.method === "OPTIONS") {
-        return context.body(null, 204);
+    const requestOrigin = context.req.header("origin");
+    const matchedOrigin = requestOrigin && cleanCommaList(CORS_ALLOWED_ORIGINS).includes(requestOrigin)
+        ? requestOrigin
+        : null;
+
+    if (matchedOrigin) {
+        context.header("Access-Control-Allow-Credentials", "true");
+        context.header("Access-Control-Allow-Headers", cleanCommaList(CORS_ALLOWED_HEADERS).join(", "));
+        context.header("Access-Control-Allow-Methods", cleanCommaList(CORS_ALLOWED_METHODS).join(", "));
+        context.header("Access-Control-Allow-Origin", matchedOrigin);
+
+        // For browser preflight requests
+        if (context.req.method === "OPTIONS") {
+            return context.body(null, 204);
+        }
     }
 
     await next();
