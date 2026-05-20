75b24920d6d95e64f7bc245a889f0b3ca4916d03
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-05-20 14:02:51 -0400

    Rename prewarming

--- src/live-streaming/PrewarmLiveEventController.ts
+++ src/live-streaming/PrewarmLiveEventController.ts
@@ -4,10 +4,10 @@ import { z } from "zod";
 import type { AppContext } from "../index";
 import { NotAValidPrewarmLiveEventPayloadError } from "./domain/model/error/NotAValidPrewarmLiveEventPayloadError";
 import { OPENAPI_TAGS } from "./config";
-import { PrewarmLiveEventDomainModelSchema } from "./domain/model/request/PrewarmLiveEventDomainModel";
 import { PrewarmingLiveEventDomainModelSchema } from "./domain/model/response/PrewarmingLiveEventDomainModel";
+import { PrewarmLiveEventDomainModelSchema } from "./domain/model/request/PrewarmLiveEventDomainModel";
+import { PrewarmLiveEventUseCase } from "./domain/usecase/PrewarmLiveEventUseCase";
 import { StatusOnlyEventMapper } from "./data/mapper/StatusOnlyEventMapper";
-import { StorePrewarmEventUseCase } from "./domain/usecase/StorePrewarmEventUseCase";
 import { UnauthorizedError } from "../shared/domain/model/error/UnauthorizedError";
 
 export class PrewarmLiveEventController extends OpenAPIRoute {
@@ -32,7 +32,7 @@ export class PrewarmLiveEventController extends OpenAPIRoute {
     };
 
     async handle(c: AppContext) {
-        const useCase = new StorePrewarmEventUseCase(
+        const useCase = new PrewarmLiveEventUseCase(
             c.env.DB,
             c.env.EVENT_CANCELLATION_EXPIRATION_JOB,
             c.env.STREAM_HUB
--- src/live-streaming/domain/usecase/PrewarmLiveEventUseCase.ts
+++ src/live-streaming/domain/usecase/PrewarmLiveEventUseCase.ts
@@ -4,7 +4,7 @@ import { EventCancellationExpirationJobDurableObject } from "../../EventCancella
 import { LiveEventDataSource } from "../../data/datasource/LiveEventDataSource";
 import { StreamHubDurableObject } from "../../StreamHubDurableObject";
 
-export class StorePrewarmEventUseCase {
+export class PrewarmLiveEventUseCase {
     constructor(
         d1: D1Database,
         eventCancellationExpirationJob: DurableObjectNamespace<EventCancellationExpirationJobDurableObject>,
